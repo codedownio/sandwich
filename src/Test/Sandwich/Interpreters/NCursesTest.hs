fc5b306e4d97f26f1f4f914071a25bfbcc2d57d1
Tom McLaughlin <tom@codedown.io>
2020-06-12 10:12:01 -0700

    Fix indentation

--- src/Test/Sandwich/Interpreters/NCursesTest.hs
+++ src/Test/Sandwich/Interpreters/NCursesTest.hs
@@ -6,34 +6,33 @@ import UI.NCurses
 
 main :: IO ()
 main = runCurses $ do
-    setEcho False
-    w <- defaultWindow
+  setEcho False
+  w <- defaultWindow
 
-    greenColorID <- newColorID ColorGreen ColorDefault 1
-    redColorID <- newColorID ColorRed ColorDefault 2
+  greenColorID <- newColorID ColorGreen ColorDefault 1
+  redColorID <- newColorID ColorRed ColorDefault 2
 
-    updateWindow w $ do
+  updateWindow w $ do
+    drawBox Nothing Nothing
 
-        drawBox Nothing Nothing
+    moveCursor 1 1
+    setColor greenColorID
+    drawString "Hello world!"
 
-        moveCursor 1 1
-        setColor greenColorID
-        drawString "Hello world!zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
+    moveCursor 3 1
+    setColor redColorID
+    drawString "(press q to quit)"
 
-        moveCursor 3 1
-        setColor redColorID
-        drawString "(press q to quit)"
+    moveCursor 4 1
+    drawLineH (Just (Glyph '-' [])) 999
 
-        moveCursor 4 1
-        drawLineH (Just (Glyph '-' [])) 999
-
-    render
-    waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
+  render
+  waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
 
 waitFor :: Window -> (Event -> Bool) -> Curses ()
 waitFor w p = loop where
-    loop = do
-        ev <- getEvent w Nothing
-        case ev of
-            Nothing -> loop
-            Just ev' -> if p ev' then return () else loop
+  loop = do
+    ev <- getEvent w Nothing
+    case ev of
+      Nothing -> loop
+      Just ev' -> if p ev' then return () else loop
