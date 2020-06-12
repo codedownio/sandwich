a6e102a61293366d75054c437b98220310a50846
Tom McLaughlin <tom@codedown.io>
2020-06-12 10:24:11 -0700

    Change "running" -> "run"

--- src/Test/Sandwich/Interpreters/RunTreeScheduler.hs
+++ src/Test/Sandwich/Interpreters/RunTreeScheduler.hs
@@ -7,66 +7,63 @@ module Test.Sandwich.Interpreters.RunTreeScheduler (runTree) where
 import Control.Monad.Free
 import Control.Monad.Trans.Reader
 import Control.Monad.IO.Class
-import Control.Concurrent.QSem
 import Control.Concurrent.Async
-import Control.Exception
 import Test.Sandwich.Types.Spec
 import Data.IORef
 import Control.Scheduler
 import Test.Sandwich.Types.Example
 import Data.Time.Clock
-import qualified Data.List as L
 
 data Status = NotStarted
             | Running UTCTime
             | Done Result
 
-type RunningTreeStatus = IORef Status
+type RunTreeStatus = IORef Status
 
-instance Show RunningTreeStatus where
+instance Show RunTreeStatus where
   show _ = "STATUS"
 
-data RunningTree =
-  RunningTreeGroupWithStatus { runningTreeLabel :: String
-                             , runningTreeStatus :: RunningTreeStatus
-                             , runningTreeChildren :: [RunningTree]
+data RunTree =
+  RunTreeGroupWithStatus { runTreeLabel :: String
+                             , runTreeStatus :: RunTreeStatus
+                             , runTreeChildren :: [RunTree]
                              }
-  | RunningTreeGroup { runningTreeLabel :: String
-                     , runningTreeChildren :: [RunningTree]
+  | RunTreeGroup { runTreeLabel :: String
+                     , runTreeChildren :: [RunTree]
                      }
-  | RunningTreeSingle { runningTreeLabel :: String
-                      , runningTreeStatus :: RunningTreeStatus
+  | RunTreeSingle { runTreeLabel :: String
+                      , runTreeStatus :: RunTreeStatus
                       }
   deriving Show
 
-runTree :: (Show r, Show context) => Free (SpecCommand context) r -> ReaderT (Async context, Scheduler IO ()) IO RunningTree
+runTree :: (Show r, Show context) => Free (SpecCommand context) r -> ReaderT (Async context, Scheduler IO ()) IO RunTree
 
 runTree (Free (Before l f subspec next)) = do
   status <- liftIO $ newIORef NotStarted
   subtree <- runTree subspec
-  return $ RunningTreeGroupWithStatus l status [subtree]
+  return $ RunTreeGroupWithStatus l status [subtree]
 
 runTree (Free (Around l f subspec next)) = do
   status <- liftIO $ newIORef NotStarted
   let asyncContext = undefined
   (_, sched) <- ask
   subtree <- withReaderT (const (asyncContext, sched)) $ runTree subspec
-  return $ RunningTreeGroupWithStatus l status [subtree]
+  return $ RunTreeGroupWithStatus l status [subtree]
 
 runTree (Free (Introduce l alloc cleanup subspec next)) = do
   status <- liftIO $ newIORef NotStarted
   let asyncContext = undefined
   (_, sched) <- ask
   subtree <- withReaderT (const (asyncContext, sched)) $ runTree subspec
-  return $ RunningTreeGroupWithStatus l status [subtree]
+  return $ RunTreeGroupWithStatus l status [subtree]
 
 runTree (Free (Describe l subspec next)) = do
   subtree <- runTree subspec
-  return $ RunningTreeGroup l [subtree]
+  return $ RunTreeGroup l [subtree]
 
 runTree (Free (DescribeParallel l subspec next)) = do
   subtree <- runTree subspec
-  return $ RunningTreeGroup l [subtree]
+  return $ RunTreeGroup l [subtree]
 
 runTree (Free (It l ex next)) = do
   -- (ctxAsync, sched) <- ask
@@ -75,11 +72,11 @@ runTree (Free (It l ex next)) = do
   --   ctx <- wait ctxAsync
   --   scheduleWork sched $ do
   --     startTime <- liftIO getCurrentTime
-  --     liftIO $ atomicWriteIORef status (Running startTime)
+  --     liftIO $ atomicWriteIORef status (Run startTime)
   --     ret <- ex ctx
   --     atomicWriteIORef status (Done ret)
 
-  return $ RunningTreeSingle l status
+  return $ RunTreeSingle l status
 
 
 runTree (Pure _) = undefined
