a5cab6bf2052039a3599bc14030879f5e515c61d
Tom McLaughlin <tom@codedown.io>
2020-07-11 22:17:45 -0700

    Able to convert spec to fixed or variable run trees

--- sandwich/src/Test/Sandwich/Interpreters/RunTree.hs
+++ sandwich/src/Test/Sandwich/Interpreters/RunTree.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -9,10 +10,12 @@
 
 module Test.Sandwich.Interpreters.RunTree (
   specToRunTree
+  , specToRunTreeVariable
   ) where
 
+import Control.Concurrent.STM
 import Control.Monad.Free
-import Control.Monad.Trans.Reader
+import Control.Monad.Trans.RWS
 import Data.Functor.Identity
 import Data.Sequence
 import Test.Sandwich.Interpreters.RunTree.Util
@@ -20,47 +23,68 @@ import Test.Sandwich.Types.RunTree
 import Test.Sandwich.Types.Spec
 
 
-specToRunTree :: BaseContext -> Free (SpecCommand BaseContext IO) () -> [RunNode BaseContext]
+specToRunTree :: BaseContext -> Free (SpecCommand BaseContext IO) () -> [RunNodeFixed BaseContext]
 specToRunTree baseContext spec = runIdentity $ specToRunTreeM baseContext spec
 
-specToRunTreeM :: (Monad m) => BaseContext -> Free (SpecCommand BaseContext IO) () -> m [RunNode BaseContext]
+specToRunTreeVariable :: BaseContext -> Free (SpecCommand BaseContext IO) () -> STM [RunNode BaseContext]
+specToRunTreeVariable bc spec = mapM unFixRunTree $ specToRunTree bc spec
+
+specToRunTreeM :: (Monad m) => BaseContext -> Free (SpecCommand BaseContext IO) () -> m [RunNodeFixed BaseContext]
 specToRunTreeM baseContext spec = do
-  runReaderT (specToRunTree' spec) $ RunTreeContext {
-    runTreeOptions = baseContextOptions baseContext
-    , runTreeIndexInParent = 0
-    , runTreeNumSiblings = countChildren spec
-    , runTreeCurrentFolder = baseContextRunRoot baseContext
-    }
+  let context = RunTreeContext {
+        runTreeOptions = baseContextOptions baseContext
+        , runTreeIndexInParent = 0
+        , runTreeNumSiblings = countChildren spec
+        , runTreeCurrentAncestors = mempty
+        , runTreeCurrentFolder = baseContextRunRoot baseContext
+        }
+  (ret, _, _) <- runRWST (specToRunTree' spec) context 0
+  return ret
 
 -- | Convert a spec to a run tree
-specToRunTree' :: (Monad m, HasBaseContext context) => Free (SpecCommand context IO) r -> ReaderT RunTreeContext m [RunNode context]
-specToRunTree' (Free (Before l f subspec next)) =
-  continueWith next =<< RunNodeBefore <$> getCommon l 100 <*> recurse l subspec <*> pure f
-specToRunTree' (Free (After l f subspec next)) =
-  continueWith next =<< RunNodeAfter <$> getCommon l 100 <*> recurse l subspec <*> pure f
-specToRunTree' (Free (Introduce l _cl alloc cleanup subspec next)) =
-  continueWith next =<< RunNodeIntroduce <$> getCommon l 100 <*> recurse l subspec <*> pure alloc <*> pure cleanup
-specToRunTree' (Free (IntroduceWith l _cl action subspec next)) =
-  continueWith next =<< RunNodeIntroduceWith <$> getCommon l 100 <*> recurse l subspec <*> pure action
-specToRunTree' (Free (Around l actionWith subspec next)) =
-  continueWith next =<< RunNodeAround <$> getCommon l 100 <*> recurse l subspec <*> pure actionWith
-specToRunTree' (Free (Describe l subspec next)) =
-  continueWith next =<< RunNodeDescribe <$> getCommon l 100 <*> recurse l subspec
-specToRunTree' (Free (Parallel subspec next)) =
-  continueWith next =<< RunNodeParallel <$> getCommon "Parallel" 100 <*> recurse "Parallel" subspec
-specToRunTree' (Free (It l example next)) =
+specToRunTree' :: (Monad m, HasBaseContext context) => Free (SpecCommand context IO) r -> ConvertM m [RunNodeFixed context]
+specToRunTree'  (Free (Before l f subspec next)) = do
+  common <- getCommon l 100
+  continueWith next =<< RunNodeBefore <$> pure common <*> recurse l common subspec <*> pure f
+specToRunTree'  (Free (After l f subspec next)) = do
+  common <- getCommon l 100
+  continueWith next =<< RunNodeAfter <$> pure common <*> recurse l common subspec <*> pure f
+specToRunTree'  (Free (Introduce l _cl alloc cleanup subspec next)) = do
+  common <- getCommon l 100
+  continueWith next =<< RunNodeIntroduce <$> pure common <*> recurse l common subspec <*> pure alloc <*> pure cleanup
+specToRunTree'  (Free (IntroduceWith l _cl action subspec next)) = do
+  common <- getCommon l 100
+  continueWith next =<< RunNodeIntroduceWith <$> pure common <*> recurse l common subspec <*> pure action
+specToRunTree'  (Free (Around l actionWith subspec next)) = do
+  common <- getCommon l 100
+  continueWith next =<< RunNodeAround <$> pure common <*> recurse l common subspec <*> pure actionWith
+specToRunTree'  (Free (Describe l subspec next)) = do
+  common <- getCommon l 100
+  continueWith next =<< RunNodeDescribe <$> pure common <*> recurse l common subspec
+specToRunTree'  (Free (Parallel subspec next)) = do
+  common <- getCommon "Parallel" 100
+  continueWith next =<< RunNodeParallel <$> pure common <*> recurse "Parallel" common subspec
+specToRunTree'  (Free (It l example next)) =
   continueWith next =<< RunNodeIt <$> getCommon l 100 <*> pure example
-specToRunTree' (Pure _) = return []
+specToRunTree'  (Pure _) = return []
 
 
 -- * Util
 
-getCommon :: (Monad m) => String -> Int -> ReaderT RunTreeContext m (RunNodeCommon Status (Seq LogEntry) Bool)
-getCommon l visibilityLevel = do
-  rtc <- ask
+type ConvertM m = RWST RunTreeContext () Int m
 
-  return $ RunNodeCommon {
+getCommon :: (Monad m) => String -> Int -> ConvertM m RunNodeCommonFixed
+getCommon l visibilityLevel = do
+  rtc@(RunTreeContext {..}) <- ask
+
+  -- Get a unique ID for this node
+  ident <- get
+  modify (+1)
+
+  return $ RunNodeCommonWithStatus {
     runTreeLabel = l
+    , runTreeId = ident
+    , runTreeAncestors = runTreeCurrentAncestors |> ident
     , runTreeToggled = False
     , runTreeStatus = NotStarted
     , runTreeFolder = appendFolder rtc l
@@ -68,10 +92,11 @@ getCommon l visibilityLevel = do
     , runTreeLogs = mempty
     }
 
-continueWith :: (Monad m, HasBaseContext context) => Free (SpecCommand context IO) r -> RunNode context -> ReaderT RunTreeContext m [RunNode context]
+continueWith :: (Monad m, HasBaseContext context) => Free (SpecCommand context IO) r -> RunNodeFixed context -> ConvertM m [RunNodeFixed context]
 continueWith next node = do
   rest <- local (\rtc -> rtc { runTreeIndexInParent = (runTreeIndexInParent rtc) + 1}) $ specToRunTree' next
   return (node : rest)
 
-recurse :: (Monad m, HasBaseContext context) => String -> Free (SpecCommand context IO) r -> ReaderT RunTreeContext m [RunNode context]
-recurse l subspec = local (\rtc -> rtc { runTreeCurrentFolder = appendFolder rtc l }) (specToRunTree' subspec)
+recurse :: (Monad m, HasBaseContext context) => String -> RunNodeCommonFixed -> Free (SpecCommand context IO) r -> ConvertM m [RunNodeFixed context]
+recurse l (RunNodeCommonWithStatus {..}) subspec = local (\rtc -> rtc { runTreeCurrentFolder = appendFolder rtc l
+                                                                      , runTreeCurrentAncestors = runTreeAncestors }) (specToRunTree' subspec)
--- sandwich/src/Test/Sandwich/Types/RunTree.hs
+++ sandwich/src/Test/Sandwich/Types/RunTree.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE NamedFieldPuns #-}
@@ -13,7 +14,6 @@ module Test.Sandwich.Types.RunTree where
 
 import Control.Concurrent.Async
 import Control.Concurrent.STM
-import Control.Monad
 import Control.Monad.Logger
 import Data.Sequence
 import Data.Time.Clock
@@ -27,8 +27,40 @@ data Status = NotStarted
                    , statusEndTime :: UTCTime
                    , statusResult :: Result }
             deriving (Show, Eq)
-data RunNodeCommon s l t = RunNodeCommon {
+
+data RunNodeWithStatus context s l t where
+  RunNodeBefore :: { runNodeCommon :: RunNodeCommonWithStatus s l t
+                   , runNodeChildren :: [RunNodeWithStatus context s l t]
+                   , runNodeBefore :: ExampleT context m () } -> RunNodeWithStatus context s l t
+  RunNodeAfter :: { runNodeCommon :: RunNodeCommonWithStatus s l t
+                  , runNodeChildren :: [RunNodeWithStatus context s l t]
+                  , runNodeAfter :: ExampleT context m () } -> RunNodeWithStatus context s l t
+  RunNodeIntroduce :: { runNodeCommon :: RunNodeCommonWithStatus s l t
+                      , runNodeChildrenAugmented :: [RunNodeWithStatus (LabelValue lab intro :> context) s l t]
+                      , runNodeAlloc :: ExampleT context m intro
+                      , runNodeCleanup :: intro -> ExampleT context m () } -> RunNodeWithStatus context s l t
+  RunNodeIntroduceWith :: { runNodeCommon :: RunNodeCommonWithStatus s l t
+                          , runNodeChildrenAugmented :: [RunNodeWithStatus (LabelValue lab intro :> context) s l t]
+                          , runNodeIntroduceAction :: ActionWith intro -> ExampleT context m () } -> RunNodeWithStatus context s l t
+  RunNodeAround :: { runNodeCommon :: RunNodeCommonWithStatus s l t
+                   , runNodeChildren :: [RunNodeWithStatus context s l t]
+                   , runNodeActionWith :: ExampleT context m Result -> ExampleT context m () } -> RunNodeWithStatus context s l t
+  RunNodeDescribe :: { runNodeCommon :: RunNodeCommonWithStatus s l t
+                     , runNodeChildren :: [RunNodeWithStatus context s l t] } -> RunNodeWithStatus context s l t
+  RunNodeParallel :: { runNodeCommon :: RunNodeCommonWithStatus s l t
+                     , runNodeChildren :: [RunNodeWithStatus context s l t] } -> RunNodeWithStatus context s l t
+  RunNodeIt :: { runNodeCommon :: RunNodeCommonWithStatus s l t
+               , runNodeExample :: ExampleT context m () } -> RunNodeWithStatus context s l t
+
+type RunNodeFixed context = RunNodeWithStatus context Status (Seq LogEntry) Bool
+type RunNode context = RunNodeWithStatus context (Var Status) (Var (Seq LogEntry)) (Var Bool)
+
+-- * RunNodeCommon
+
+data RunNodeCommonWithStatus s l t = RunNodeCommonWithStatus {
   runTreeLabel :: String
+  , runTreeId :: Int
+  , runTreeAncestors :: Seq Int
   , runTreeToggled :: t
   , runTreeStatus :: s
   , runTreeFolder :: Maybe FilePath
@@ -36,29 +68,10 @@ data RunNodeCommon s l t = RunNodeCommon {
   , runTreeLogs :: l
   } deriving Show
 
-data RunNodeWithStatus context s l t where
-  RunNodeBefore :: { runNodeCommon :: RunNodeCommon s l t
-                   , runNodeChildren :: [RunNodeWithStatus context s l t]
-                   , runNodeBefore :: ExampleT context m () } -> RunNodeWithStatus context s l t
-  RunNodeAfter :: { runNodeCommon :: RunNodeCommon s l t
-                  , runNodeChildren :: [RunNodeWithStatus context s l t]
-                  , runNodeAfter :: ExampleT context m () } -> RunNodeWithStatus context s l t
-  RunNodeIntroduce :: { runNodeCommon :: RunNodeCommon s l t
-                      , runNodeChildrenAugmented :: [RunNodeWithStatus (LabelValue lab intro :> context) s l t]
-                      , runNodeAlloc :: ExampleT context m intro
-                      , runNodeCleanup :: intro -> ExampleT context m () } -> RunNodeWithStatus context s l t
-  RunNodeIntroduceWith :: { runNodeCommon :: RunNodeCommon s l t
-                          , runNodeChildrenAugmented :: [RunNodeWithStatus (LabelValue lab intro :> context) s l t]
-                          , runNodeIntroduceAction :: ActionWith intro -> ExampleT context m () } -> RunNodeWithStatus context s l t
-  RunNodeAround :: { runNodeCommon :: RunNodeCommon s l t
-                   , runNodeChildren :: [RunNodeWithStatus context s l t]
-                   , runNodeActionWith :: ExampleT context m Result -> ExampleT context m () } -> RunNodeWithStatus context s l t
-  RunNodeDescribe :: { runNodeCommon :: RunNodeCommon s l t
-                     , runNodeChildren :: [RunNodeWithStatus context s l t] } -> RunNodeWithStatus context s l t
-  RunNodeParallel :: { runNodeCommon :: RunNodeCommon s l t
-                     , runNodeChildren :: [RunNodeWithStatus context s l t] } -> RunNodeWithStatus context s l t
-  RunNodeIt :: { runNodeCommon :: RunNodeCommon s l t
-               , runNodeExample :: ExampleT context m () } -> RunNodeWithStatus context s l t
+type RunNodeCommonFixed = RunNodeCommonWithStatus Status (Seq LogEntry) Bool
+type RunNodeCommon = RunNodeCommonWithStatus (Var Status) (Var (Seq LogEntry)) (Var Bool)
+
+-- * Other
 
 type Var = TVar
 data LogEntry = LogEntry { logEntryTime :: UTCTime
@@ -68,14 +81,11 @@ data LogEntry = LogEntry { logEntryTime :: UTCTime
                          , logEntryStr :: LogStr
                          } deriving (Show, Eq)
 
-type RunNode context = RunNodeWithStatus context Status (Seq LogEntry) Bool
--- type RunNode context = RunNodeWithStatus context (Var Status) (Var (Seq LogEntry)) (Var Bool)
--- type RunTreeFixed = RunTreeWithStatus Status (Seq LogEntry) Bool
-
 -- | Context passed around through the evaluation of a RunTree
 data RunTreeContext = RunTreeContext {
   runTreeOptions :: Options
   , runTreeCurrentFolder :: Maybe FilePath
+  , runTreeCurrentAncestors :: Seq Int
   , runTreeIndexInParent :: Int
   , runTreeNumSiblings :: Int
   }
@@ -84,21 +94,82 @@ isFailureStatus :: Status -> Bool
 isFailureStatus (Done _ _ stat) = isFailure stat
 isFailureStatus _ = False
 
--- fixRunTree :: RunTree -> STM RunTreeFixed
--- fixRunTree (RunTreeNode {..}) = do
---   status <- readTVar runTreeStatus
---   logs <- readTVar runTreeLogs
---   toggled <- readTVar runTreeToggled
+fixRunTree :: RunNode context -> STM (RunNodeFixed context)
+fixRunTree node@(runNodeCommon -> (RunNodeCommonWithStatus {..})) = do
+  status <- readTVar runTreeStatus
+  logs <- readTVar runTreeLogs
+  toggled <- readTVar runTreeToggled
 
---   children <- forM runTreeChildren fixRunTree
+  let common' = RunNodeCommonWithStatus {
+        runTreeStatus = status
+        , runTreeLogs = logs
+        , runTreeToggled = toggled
+        , ..
+        }
 
---   return $ RunTreeNode {
---     runTreeStatus = status
---     , runTreeLogs = logs
---     , runTreeToggled = toggled
---     , runTreeChildren = children
---     , ..
---     }
+  case node of
+    RunNodeBefore {..} -> do
+      children <- mapM fixRunTree runNodeChildren
+      return $ RunNodeBefore { runNodeCommon=common', runNodeChildren=children, .. }
+    RunNodeAfter {..} -> do
+      children <- mapM fixRunTree runNodeChildren
+      return $ RunNodeAfter { runNodeCommon=common', runNodeChildren=children, .. }
+    RunNodeIntroduce {..} -> do
+      children <- mapM fixRunTree runNodeChildrenAugmented
+      return $ RunNodeIntroduce { runNodeCommon=common', runNodeChildrenAugmented=children, .. }
+    RunNodeIntroduceWith {..} -> do
+      children <- mapM fixRunTree runNodeChildrenAugmented
+      return $ RunNodeIntroduceWith { runNodeCommon=common', runNodeChildrenAugmented=children, .. }
+    RunNodeAround {..} -> do
+      children <- mapM fixRunTree runNodeChildren
+      return $ RunNodeAround { runNodeCommon=common', runNodeChildren=children, .. }
+    RunNodeDescribe {..} -> do
+      children <- mapM fixRunTree runNodeChildren
+      return $ RunNodeDescribe { runNodeCommon=common', runNodeChildren=children, .. }
+    RunNodeParallel {..} -> do
+      children <- mapM fixRunTree runNodeChildren
+      return $ RunNodeParallel { runNodeCommon=common', runNodeChildren=children, .. }
+    RunNodeIt {..} -> do
+      return $ RunNodeIt { runNodeCommon=common', .. }
+
+unFixRunTree :: RunNodeFixed context -> STM (RunNode context)
+unFixRunTree node@(runNodeCommon -> (RunNodeCommonWithStatus {..})) = do
+  status <- newTVar runTreeStatus
+  logs <- newTVar runTreeLogs
+  toggled <- newTVar runTreeToggled
+
+  let common' = RunNodeCommonWithStatus {
+        runTreeStatus = status
+        , runTreeLogs = logs
+        , runTreeToggled = toggled
+        , ..
+        }
+
+  case node of
+    RunNodeBefore {..} -> do
+      children <- mapM unFixRunTree runNodeChildren
+      return $ RunNodeBefore { runNodeCommon=common', runNodeChildren=children, .. }
+    RunNodeAfter {..} -> do
+      children <- mapM unFixRunTree runNodeChildren
+      return $ RunNodeAfter { runNodeCommon=common', runNodeChildren=children, .. }
+    RunNodeIntroduce {..} -> do
+      children <- mapM unFixRunTree runNodeChildrenAugmented
+      return $ RunNodeIntroduce { runNodeCommon=common', runNodeChildrenAugmented=children, .. }
+    RunNodeIntroduceWith {..} -> do
+      children <- mapM unFixRunTree runNodeChildrenAugmented
+      return $ RunNodeIntroduceWith { runNodeCommon=common', runNodeChildrenAugmented=children, .. }
+    RunNodeAround {..} -> do
+      children <- mapM unFixRunTree runNodeChildren
+      return $ RunNodeAround { runNodeCommon=common', runNodeChildren=children, .. }
+    RunNodeDescribe {..} -> do
+      children <- mapM unFixRunTree runNodeChildren
+      return $ RunNodeDescribe { runNodeCommon=common', runNodeChildren=children, .. }
+    RunNodeParallel {..} -> do
+      children <- mapM unFixRunTree runNodeChildren
+      return $ RunNodeParallel { runNodeCommon=common', runNodeChildren=children, .. }
+    RunNodeIt {..} -> do
+      return $ RunNodeIt { runNodeCommon=common', .. }
+
 
 getCallStackFromStatus :: Status -> Maybe CallStack
 getCallStackFromStatus NotStarted {} = Nothing
