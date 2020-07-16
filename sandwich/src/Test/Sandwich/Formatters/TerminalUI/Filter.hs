2e2e0bfa64d97e25cad857bb443d94419390b139
Tom McLaughlin <tom@codedown.io>
2020-07-15 17:45:12 -0700

    Check in WIP filterRunTree changes

--- sandwich/src/Test/Sandwich/Formatters/TerminalUI/Filter.hs
+++ sandwich/src/Test/Sandwich/Formatters/TerminalUI/Filter.hs
@@ -8,15 +8,21 @@ module Test.Sandwich.Formatters.TerminalUI.Filter (
 import Data.Function
 import Test.Sandwich.Types.RunTree
 
-filterRunTree :: Bool -> [RunNodeWithStatus context a l t] -> [RunNodeWithStatus context a l t]
-filterRunTree showContextManagers rtsFixed = rtsFixed
+filterRunTree :: Int -> [RunNodeWithStatus context s l t] -> [RunNodeWithStatus context s l t]
+filterRunTree visibilityThreshold rtsFixed = rtsFixed
   & id
-  -- & if showContextManagers then id else filterContextManagers
+  -- (filterVisibilityThreshold visibilityThreshold)
 
--- filterContextManagers :: [RunNodeWithStatus a l t] -> [RunNodeWithStatus a l t]
--- filterContextManagers = mconcat . fmap filterContextManagersSingle
+-- filterVisibilityThreshold :: Int -> [RunNodeWithStatus context s l t] -> [RunNodeWithStatus context s l t]
+-- filterVisibilityThreshold thresh = mconcat . fmap (filterVisibilityThresholdSingle thresh)
 
--- filterContextManagersSingle :: RunNodeWithStatus a l t -> [RunNodeWithStatus a l t]
--- filterContextManagersSingle rt@(RunTreeGroup {runTreeIsContextManager=False, ..}) = [rt { runTreeChildren = filterContextManagers runTreeChildren }]
--- filterContextManagersSingle (RunTreeGroup {runTreeIsContextManager=True, ..}) = filterContextManagers runTreeChildren
--- filterContextManagersSingle rt@(RunTreeSingle {}) = [rt]
+-- filterVisibilityThresholdSingle :: Int -> RunNodeWithStatus context s l t -> [RunNodeWithStatus context s l t]
+-- filterVisibilityThresholdSingle thresh node | thresh < runTreeVisibilityLevel (runNodeCommon node) = case node of
+--                                                 RunNodeIt {} -> []
+--                                                 RunNodeIntroduce {..} -> filterVisibilityThreshold thresh runNodeChildrenAugmented
+
+-- filterVisibilityThresholdSingle thresh node@(RunNodeIntroduce {..}) = [RunNodeIntroduce { runNodeChildrenAugmented = filterVisibilityThreshold thresh runNodeChildrenAugmented, .. }]
+-- filterVisibilityThresholdSingle thresh node = [node { runNodeChildren = filterVisibilityThreshold thresh (runNodeChildren node) }]
+--   = [rt { runTreeChildren = filterVisibilityThreshold runTreeChildren }]
+-- filterVisibilityThresholdSingle thresh (RunTreeGroup {runTreeIsContextManager=True, ..}) = filterVisibilityThreshold runTreeChildren
+-- filterVisibilityThresholdSingle thresh rt@(RunTreeSingle {}) = [rt]
