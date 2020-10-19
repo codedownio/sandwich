5990fe014c73fdcadcc85f945611c176f44a66c4
Tom McLaughlin <tom@codedown.io>
2020-10-19 13:01:37 -0600

    Finish toMarkdown function in sandwich-slack formatter

--- sandwich-slack/src/Test/Sandwich/Formatters/Internal/Markdown.hs
+++ sandwich-slack/src/Test/Sandwich/Formatters/Internal/Markdown.hs
@@ -15,31 +15,25 @@ import Test.Sandwich.Formatters.Internal.Types
 
 
 toMarkdown :: FailureReason -> T.Text
-toMarkdown (Reason {..}) = markdownBlock (T.pack failureReason)
-toMarkdown (ExpectedButGot {..}) = markdownBlock [i|Expected *#{failureValue1}* but got *#{failureValue2}*|]
-toMarkdown (DidNotExpectButGot {..}) = markdownBlock [i|Did not expect *#{failureValue1}*|]
-toMarkdown (GotException {..}) = ""
-toMarkdown (Pending {..}) = ""
-toMarkdown (GetContextException {..}) = ""
-toMarkdown (GotAsyncException {..}) = ""
-
-
+toMarkdown (Reason {..}) = T.pack failureReason
+toMarkdown (ExpectedButGot {..}) = [i|Expected *#{failureValue1}* but got *#{failureValue2}*|]
+toMarkdown (DidNotExpectButGot {..}) = [i|Did not expect *#{failureValue1}*|]
+toMarkdown (GotException {..}) = case failureMessage of
+  Just msg -> [i|Got exception (_#{msg}_): #{failureException}|]
+  Nothing -> [i|Got exception: #{failureException}|]
+toMarkdown (Pending {..}) = "Example was pending"
+toMarkdown (GetContextException {..}) = [i|Context exception: #{failureException}|]
+toMarkdown (GotAsyncException {..}) = case failureMessage of
+  Just msg -> [i|Got async exception (_#{msg}_): #{failureAsyncException}|]
+  Nothing -> [i|Got async exception: #{failureAsyncException}|]
 
 callStackToMarkdown :: SlackFormatterShowCallStacks -> CallStack -> T.Text
 callStackToMarkdown SlackFormatterNoCallStacks cs = ""
 callStackToMarkdown (SlackFormatterTopNCallStackFrames n) cs = "\n\n" <> showCallStack (fromCallSiteList $ L.take n $ getCallStack cs)
 callStackToMarkdown SlackFormatterFullCallStack cs = "\n\n" <> showCallStack cs
 
-
-markdownBlock text = text
-
 showCallStack cs = prettyCallStack cs
   & T.pack
   & T.lines
   & fmap ("> " <>)
   & T.intercalate "\n"
-
--- markdownBlock text = A.object [
---   ("type", A.String "mrkdwn")
---   , ("text", A.String $ "\n" <> text)
---   ]
