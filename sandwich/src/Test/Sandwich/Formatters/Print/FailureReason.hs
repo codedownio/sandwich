779446e370c692b0e57bff4918907ac87e56e393
Tom McLaughlin <tom@codedown.io>
2020-12-30 15:14:39 -0700

    Fix failure reason inside exception printing hopefully

--- sandwich/src/Test/Sandwich/Formatters/Print/FailureReason.hs
+++ sandwich/src/Test/Sandwich/Formatters/Print/FailureReason.hs
@@ -1,11 +1,14 @@
+-- | Pretty printing failure reasons
+
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE QuasiQuotes #-}
--- | Pretty printing failure reason
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Test.Sandwich.Formatters.Print.FailureReason (
   printFailureReason
   ) where
 
+import Control.Exception.Safe
 import Control.Monad.Reader
 import qualified Data.List as L
 import Data.String.Interpolate.IsString
@@ -19,6 +22,7 @@ import Test.Sandwich.Types.Spec
 import Text.Show.Pretty as P
 
 
+printFailureReason :: FailureReason -> ReaderT (PrintFormatter, Int, Handle) IO ()
 printFailureReason (Reason _ s) = do
   printShowBoxPrettyWithTitle "Reason: " (SEB s)
 printFailureReason (ExpectedButGot _ seb1 seb2) = do
@@ -26,14 +30,23 @@ printFailureReason (ExpectedButGot _ seb1 seb2) = do
   printShowBoxPrettyWithTitle "But got: " seb2
 printFailureReason (DidNotExpectButGot _ seb) = do
   printShowBoxPrettyWithTitle "Did not expect: " seb
-printFailureReason (GotException _ maybeMessage e) = case maybeMessage of
-  Nothing -> printShowBoxPrettyWithTitle "Got exception: " (SEB e)
-  Just s -> printShowBoxPrettyWithTitle [i|Got exception (#{s})|] (SEB e)
+printFailureReason (GotException _ maybeMessage e@(SomeExceptionWithEq baseException)) =
+  case fromException baseException of
+    Just (fr :: FailureReason) -> do
+      picn midWhite "Got exception:"
+      printFailureReason fr
+    _ -> case maybeMessage of
+      Nothing -> printShowBoxPrettyWithTitle "Got exception: " (SEB e)
+      Just s -> printShowBoxPrettyWithTitle [i|Got exception (#{s})|] (SEB e)
 printFailureReason (Pending _ maybeMessage) = case maybeMessage of
   Nothing -> return () -- Just allow the yellow heading to show the pending state
   Just s -> printShowBoxPrettyWithTitle "Pending reason: " (SEB s)
-printFailureReason (GetContextException _ e) = do
-  printShowBoxPrettyWithTitle "Context exception: " (SEB e)
+printFailureReason (GetContextException _ e@(SomeExceptionWithEq baseException)) = do
+  case fromException baseException of
+    Just (fr :: FailureReason) -> do
+      picn midWhite "Context exception:"
+      printFailureReason fr
+    _ -> printShowBoxPrettyWithTitle "Context exception: " (SEB e)
 printFailureReason (GotAsyncException _ maybeMessage e) = case maybeMessage of
   Nothing -> printShowBoxPrettyWithTitle "Async exception" (SEB e)
   Just s -> printShowBoxPrettyWithTitle [i|Async exception (#{e}) |] (SEB s)
