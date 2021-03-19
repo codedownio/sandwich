3c3044a661546a63eab41f36517e452be04d98f6
Tom McLaughlin <tom@codedown.io>
2021-03-19 11:42:05 -0700

    Tidy tests

--- sandwich/test/Main.hs
+++ sandwich/test/Main.hs
@@ -1,34 +1,13 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import Control.Concurrent
-import Control.Concurrent.Async
-import Control.Concurrent.STM
-import qualified Control.Exception as E
-import Control.Exception.Safe
-import Control.Monad.IO.Class
-import Control.Monad.Logger
-import Control.Monad.Trans.Writer
-import Data.Either
-import Data.Foldable
-import qualified Data.List as L
-import Data.String.Interpolate
-import GHC.Stack
-import System.Exit
-import Test.Sandwich
-import Test.Sandwich.Internal
-
-import qualified Around as Around
-import qualified Before as Before
-import qualified Describe as Describe
-import qualified Introduce as Introduce
+import qualified Around
+import qualified Before
+import qualified Describe
+import qualified Introduce
 import TestUtil
 
 
