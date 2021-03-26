51c1dd2497369ea2eb06ef5ec3cbf3b82699aec4
Tom McLaughlin <tom@codedown.io>
2021-03-25 23:06:40 -0700

    Add getCommandLineOptions function

--- sandwich/src/Test/Sandwich/Contexts.hs
+++ sandwich/src/Test/Sandwich/Contexts.hs
@@ -6,6 +6,7 @@ module Test.Sandwich.Contexts where
 
 import Control.Monad.Reader
 import GHC.Stack
+import Test.Sandwich.Types.ArgParsing
 import Test.Sandwich.Types.RunTree
 import Test.Sandwich.Types.Spec
 
@@ -23,3 +24,9 @@ getRunRoot = asks (baseContextRunRoot . getBaseContext)
 -- not to create a folder.
 getCurrentFolder :: (HasBaseContext context, MonadReader context m, MonadIO m) => m (Maybe FilePath)
 getCurrentFolder = asks (baseContextPath . getBaseContext)
+
+-- | Get the command line options, if configured.
+-- Using the runSandwichWithCommandLineArgs family of main functions will introduce these, or you can
+-- introduce them manually
+getCommandLineOptions :: (HasCommandLineOptions context a, MonadReader context m, MonadIO m) => m (CommandLineOptions a)
+getCommandLineOptions = getContext commandLineOptions
