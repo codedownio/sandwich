fb1b1fccf900544dc97505a4dcc896ba23e73e81
Tom McLaughlin <tom@codedown.io>
2021-03-26 17:14:59 -0700

    Export ShouldWarnOnParseError(..)

--- sandwich/src/Test/Sandwich/TH.hs
+++ sandwich/src/Test/Sandwich/TH.hs
@@ -14,6 +14,7 @@ module Test.Sandwich.TH (
   , GetSpecFromFolderOptions
   , getSpecIndividualSpecHooks
   , getSpecWarnOnParseError
+  , ShouldWarnOnParseError(..)
 
   , buildModuleMap
   ) where
