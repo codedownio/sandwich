49fa2d230c82ba743cee4396f7f44959373d536a
Tom McLaughlin <tom@codedown.io>
2021-04-24 23:11:12 -0700

    Add landing page demo

---
+++ sandwich-demos/demos/landing/Main.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Main where
+
+import Common
+import Control.Concurrent
+import Control.Monad.IO.Class
+import Data.Time.Clock
+import System.Random
+import Test.Sandwich
+
+
+landingDemo :: TopSpec
+landingDemo = describe "Arithmetic tests" $ parallel $ do
+  describe "Addition" $ do
+    it "basic addition" $ do
+      (2 + 2) `shouldBe` 4
+      sleepRandom
+
+    it "adding zero" $ do
+      (2 + 0) `shouldBe` 2
+      sleepRandom
+
+    it "adding one" $ do
+      sleepRandom
+      warn "Having some trouble getting this test to pass..."
+      (0 + 1) `shouldBe` 0
+
+  describe "Multiplication" $ do
+    it "small numbers" $ do
+      (2 * 3) `shouldBe` 6
+      sleepRandom
+
+    it "multiplying by zero" $ do
+      (2 * 0) `shouldBe` 0
+      sleepRandom
+
+    it "multiplying by one" $ do
+      (2 * 1) `shouldBe` 2
+      sleepRandom
+
+    it "squaring" $ do
+      (2 * 2) `shouldBe` 4
+      sleepRandom
+
+
+sleepRandom :: ExampleM context ()
+sleepRandom = liftIO $ do
+  timeToSleep :: Int <- randomRIO (1500000, 3000000)
+  threadDelay timeToSleep
+
+testOptions = defaultOptions {
+  optionsTestArtifactsDirectory = TestArtifactsGeneratedDirectory "test_runs" (show <$> getCurrentTime)
+  }
+
+main :: IO ()
+main = runSandwichWithCommandLineArgs testOptions landingDemo
