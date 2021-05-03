9afc542213f8cb38a35ef366caa0fe78813bae1a
Tom McLaughlin <tom@codedown.io>
2021-05-02 18:00:09 -0700

    Move quickcheck demo to sandwich-demos

--- sandwich-demos/demos/quickcheck/Main.hs
+++ sandwich-demos/demos/quickcheck/Main.hs
@@ -9,13 +9,13 @@ import Data.String.Interpolate
 import Data.Time.Clock
 import Test.QuickCheck
 import Test.Sandwich
+import Test.Sandwich.QuickCheck
 
 
 quickCheckDemo :: TopSpec
-quickCheckDemo = describe "QuickCheck tests" $ do
-  it "Tests a thing" $ do
-    result <- liftIO $ quickCheckWithResult stdArgs $ \(xs :: [Int]) -> reverse xs == xs
-    info [i|Got result: #{result}|]
+quickCheckDemo = describe "QuickCheck tests" $ introduceQuickCheck $ do
+  prop "List reversal" $ \(xs :: [Int]) -> reverse (reverse xs) == xs
+  prop "Failing list reversal" $ \(xs :: [Int]) -> (reverse xs) == xs
 
 testOptions = defaultOptions {
   optionsTestArtifactsDirectory = TestArtifactsGeneratedDirectory "test_runs" (show <$> getCurrentTime)
