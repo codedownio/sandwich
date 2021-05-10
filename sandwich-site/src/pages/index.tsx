2dbaf10b057fc437be8c3ce3522fd85444259fb5
Tom McLaughlin <tom@codedown.io>
2021-05-10 15:21:33 -0600

    Site progress

--- sandwich-site/src/pages/index.tsx
+++ sandwich-site/src/pages/index.tsx
@@ -1,11 +1,13 @@
 
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import Typist from "react-typist";
+
+import styles from "./styles.module.css";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
@@ -13,24 +15,24 @@ import { Carousel } from "react-responsive-carousel";
 
 const features = [
   {
-    title: 'Powerful formatters',
-    imageUrl: 'img/print_formatter.png',
+    title: "Powerful formatters",
+    imageUrl: "img/print_formatter.png",
     description: (
       <>
           Use the terminal UI formatter to inspect test results, re-run selected tests, or jump straight to test sources in your editor. Or, choose one of the more traditional formatters.
       </>
     ),
   }, {
-    title: 'Easy profiling',
-    imageUrl: 'img/speedscope.png',
+    title: "Easy profiling",
+    imageUrl: "img/speedscope.png",
     description: (
       <>
           Automatically record timing data for every test suite and visualize it as a flamegraph.
       </>
     ),
   }, {
-    title: 'Zero-config Selenium tests',
-    imageUrl: 'img/selenium_logo.png',
+    title: "Zero-config Selenium tests",
+    imageUrl: "img/selenium_logo.png",
     description: (
       <>
           Automatically obtain Selenium driver binaries to match your Chrome or Firefox version, run tests in graphical or headless mode, and even record videos.
@@ -42,7 +44,7 @@ const features = [
 function Feature({imageUrl, title, description}) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--4', styles.feature)}>
+    <div className={clsx("col col--4", styles.feature)}>
         {imgUrl && (
           <div className="text--center">
               <img className={styles.featureImage} src={imgUrl} alt={title} />
@@ -58,18 +60,15 @@ function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <Layout title={`Hello from ${siteConfig.title}`}
+            description="Yet another test framework for Haskell.">
+        <header className={styles.heroBanner}>
             <div className="container">
                 <h1 className="hero__title">{siteConfig.title}</h1>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
 
-                <div style={{
-
-                }}>
-                    <Carousel>
+                <div className={styles.carouselContainer}>
+                    <Carousel thumbWidth={200}>
                         <div>
                             <div className={styles.carouselHeading}>Terminal UI interface</div>
                             <img src={useBaseUrl("/img/basic.gif")} />
@@ -88,15 +87,39 @@ function Home() {
                 <div className={styles.buttons}>
                     <Link
                       className={clsx(
-                        'button button--outline button--secondary button--lg',
+                        "button button--outline button--secondary button--lg",
                         styles.getStarted,
                       )}
-                      to={useBaseUrl('docs/')}>
-                        Get Started
+                      to={useBaseUrl("docs/")}>
+                        Read the docs
                     </Link>
-                </div>                
+                </div>
             </div>
         </header>
+
+        <div className={styles.section}>
+            <div className={styles.container}>
+                <span className={styles.heading}>
+                    Run the demo from the landing page
+                </span>
+
+                <div className={styles.code}>
+                    <Typist avgTypingDelay={10}>
+                        git clone git@github.com:codedownio/sandwich.git
+                        <br />
+                        cd sandwich
+                        <br />
+                        stack run landing -- --tui
+                    </Typist>
+                </div>
+
+                <span className={styles.heading}>
+                    Or, try one of the other <a className={styles.sectionLink}
+                                                href="https://github.com/codedownio/sandwich/tree/master/sandwich-demos/demos">examples</a>.
+                </span>
+            </div>
+        </div>
+
         <main>
             {features && features.length > 0 && (
               <section className={styles.features}>
