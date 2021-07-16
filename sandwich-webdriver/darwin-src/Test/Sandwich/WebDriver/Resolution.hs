ca71603185c8eff5ed763882ad980cdd1a0449ba
Tom McLaughlin <pyro777@gmail.com>
2021-07-16 12:37:08 -0700

    Final touches on resolution stuff

--- sandwich-webdriver/darwin-src/Test/Sandwich/WebDriver/Resolution.hs
+++ sandwich-webdriver/darwin-src/Test/Sandwich/WebDriver/Resolution.hs
@@ -13,12 +13,17 @@ import Data.Word
 getResolution :: IO (Int, Int, Int, Int)
 getResolution = do
   ddid <- cg_main_display_id
-  width <- cg_display_pixels_wide ddid
-  height <- cg_display_pixels_high ddid
-  return (0, 0, fromIntegral width, fromIntegral height)
+  getResolutionForDisplay (fromIntegral ddid)
 
+-- TODO: currently we just use the "main display" ID.
+-- When when we return 0 for x and y here, the WebDriver setWindowPos
+-- should just position the window on the main display.
+-- This probably won't work at all for xvfb displays, but we have more work to do there...
 getResolutionForDisplay :: Int -> IO (Int, Int, Int, Int)
-getResolutionForDisplay = undefined
+getResolutionForDisplay ddid = do
+  width <- cg_display_pixels_wide (fromIntegral ddid)
+  height <- cg_display_pixels_high (fromIntegral ddid)
+  return (0, 0, fromIntegral width, fromIntegral height)
 
 type CGDirectDisplayID = Word32
 
