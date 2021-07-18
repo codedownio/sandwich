7c01820f79bc77abcbc2b1935dff3e902d636ffc
Tom McLaughlin <tom@codedown.io>
2021-07-17 20:26:37 -0700

    Split demos up to work around how stack wants to build every executable (+ clean prune-juice run)

---
+++ demo-webdriver-pool/lib/Common.hs
@@ -0,0 +1,23 @@
+
+module Common where
+
+import Control.Concurrent
+import Control.Monad.Catch
+import Control.Monad.IO.Class
+import System.Random
+import Test.Sandwich
+
+
+pauseSeconds :: (MonadIO m) => Double -> m ()
+pauseSeconds s = liftIO $ threadDelay $ round (s * 1000000)
+
+pauseRandomAndSucceed :: (MonadIO m, MonadThrow m) => ExampleT context m ()
+pauseRandomAndSucceed = pauseRandom >> 2 `shouldBe` 2
+
+pauseRandomAndFail :: (MonadIO m, MonadThrow m) => ExampleT context m ()
+pauseRandomAndFail = pauseRandom >> 2 `shouldBe` 3
+
+pauseRandom :: (MonadIO m) => m ()
+pauseRandom = liftIO $ do
+  pauseTime <- randomRIO (1000000, 4000000)
+  threadDelay pauseTime
--- sandwich-demos/ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for sandwich-webdriver
-
-## Unreleased changes
