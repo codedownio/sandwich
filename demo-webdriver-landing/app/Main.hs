05e3cc03e00ea759e312a9bad8b9a62d3d69c8b9
Tom McLaughlin <tom@codedown.io>
2021-07-22 15:55:38 -0700

    Check in WIP webdriver-landing demo

---
+++ demo-webdriver-landing/app/Main.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Main where
+
+import Control.Concurrent
+import Control.Monad.IO.Class
+import Data.String.Interpolate
+import Data.Time.Clock
+import System.FilePath
+import Test.Sandwich
+import Test.Sandwich.WebDriver
+import Test.Sandwich.WebDriver.Windows
+import Test.WebDriver.Commands
+
+
+simple :: TopSpecWithOptions
+simple = introduceWebDriverOptions @() (defaultWdOptions "/tmp/tools") $ do
+  before "Position window" (withSession1 setWindowRightSide) $ do
+    it "opens Google" $ withSession1 $ do
+      openPage [i|https://www.google.com|]
+      liftIO $ threadDelay 2000000
+
+    it "opens Yahoo" $ withSession1 $ do
+      openPage [i|https://www.yahoo.com|]
+      liftIO $ threadDelay 2000000
+
+    it "opens Bing" $ withSession1 $ do
+      openPage [i|https://www.bing.com|]
+      liftIO $ threadDelay 2000000
+
+testOptions = defaultOptions {
+  optionsTestArtifactsDirectory = TestArtifactsGeneratedDirectory "test_runs" (show <$> getCurrentTime)
+  }
+
+main :: IO ()
+main = runSandwichWithCommandLineArgs testOptions simple
