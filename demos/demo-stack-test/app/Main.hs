ae70ca22e94c5df8fefa47db731491f85b14101e
Tom McLaughlin <tom@codedown.io>
2022-10-07 20:10:30 -0600

    Add demo-stack-test

---
+++ demos/demo-stack-test/app/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Control.Concurrent
+import Control.Monad.IO.Class
+import Test.Sandwich
+
+basic :: TopSpec
+basic = describe "Simple tests" $ do
+  describe "Arithmetic" $ do
+    it "adds" $ do
+      (2 + 2) `shouldBe` 4
+      (2 + 3) `shouldBe` 5
+
+    it "subtracts" $ do
+      warn "This might not be right..."
+      (3 - 2) `shouldBe` 0
+
+  describe "Strings" $
+    it "concatenates" $
+      ("abc" <> "def") `shouldBe` "abcdef"
+
+main :: IO ()
+main = runSandwichWithCommandLineArgs defaultOptions basic
