9b006323acc256d4be963f42b6333c1bedebad8e
thomasjm <tom@codedown.io>
2023-11-19 02:47:22 -0800

    More refactoring in RunTimes.hs

--- sandwich/src/Test/Sandwich/Formatters/TerminalUI/Draw/RunTimes.hs
+++ sandwich/src/Test/Sandwich/Formatters/TerminalUI/Draw/RunTimes.hs
@@ -17,23 +17,23 @@ maxGray :: Int = 255
 
 getRunTimes app startTime endTime statusSetupTime statusTeardownTime showEllipses = raw setupWork <+> raw actualWork <+> raw teardownWork
   where
-    totalElapsed = realToFrac (diffUTCTime (app ^. appCurrentTime) (app ^. appStartTime))
-
-    getLevel :: Double -> Int
-    getLevel duration = min maxGray $ max minGray $ round (fromIntegral minGray + (intensity * (fromIntegral (maxGray - minGray))))
-      where
-        intensity :: Double = logBase (totalElapsed + 1) (duration + 1)
-
-    getAttr :: NominalDiffTime -> V.Attr
-    getAttr dt = V.Attr {
-      V.attrStyle = V.Default
-      , V.attrForeColor = V.SetTo $ grayAt $ getLevel (realToFrac dt)
-      , V.attrBackColor = V.Default
-      , V.attrURL = V.Default
-      }
+    totalElapsed = diffUTCTime (app ^. appCurrentTime) (app ^. appStartTime)
 
     actualWorkTime = (diffUTCTime endTime startTime) - (fromMaybe 0 statusSetupTime) - (fromMaybe 0 statusTeardownTime)
 
-    setupWork = maybe mempty (\dt -> V.string (getAttr dt) [i|(#{formatNominalDiffTime dt}) + |]) statusSetupTime
-    actualWork = V.string (getAttr actualWorkTime) (formatNominalDiffTime actualWorkTime <> (if showEllipses then "..." else ""))
-    teardownWork = maybe mempty (\dt -> V.string (getAttr dt) [i| + (#{formatNominalDiffTime dt})|]) statusTeardownTime
+    setupWork = maybe mempty (\dt -> V.string (getAttr totalElapsed dt) [i|(#{formatNominalDiffTime dt}) + |]) statusSetupTime
+    actualWork = V.string (getAttr totalElapsed actualWorkTime) (formatNominalDiffTime actualWorkTime <> (if showEllipses then "..." else ""))
+    teardownWork = maybe mempty (\dt -> V.string (getAttr totalElapsed dt) [i| + (#{formatNominalDiffTime dt})|]) statusTeardownTime
+
+getAttr :: NominalDiffTime -> NominalDiffTime -> V.Attr
+getAttr totalElapsed dt = V.Attr {
+  V.attrStyle = V.Default
+  , V.attrForeColor = V.SetTo $ grayAt $ getLevel (realToFrac totalElapsed) (realToFrac dt)
+  , V.attrBackColor = V.Default
+  , V.attrURL = V.Default
+  }
+
+getLevel :: Double -> Double -> Int
+getLevel totalElapsed duration = min maxGray $ max minGray $ round (fromIntegral minGray + (intensity * (fromIntegral (maxGray - minGray))))
+  where
+    intensity :: Double = logBase (totalElapsed + 1) (duration + 1)
