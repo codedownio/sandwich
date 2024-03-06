a6ab2942e70111d055702953ad2636a2bf20f294
Tom McLaughlin <tom@codedown.io>
2024-03-06 01:57:57 -0800

    demo-nix-binary: use helper function to show signature

--- demos/demo-nix-binary/app/Main.hs
+++ demos/demo-nix-binary/app/Main.hs
@@ -5,6 +5,7 @@ module Main where
 
 import Control.Concurrent
 import Control.Monad.IO.Class
+import Control.Monad.Reader
 import Sandwich.Contexts.Files
 import Sandwich.Contexts.Nix
 import Test.Sandwich
@@ -13,10 +14,16 @@ import UnliftIO.Process
 
 spec :: TopSpec
 spec = describe "Introducing a Nix binary" $
-  introduceNixContext nixpkgsReleaseDefault $ introduceBinaryViaNixPackage @"hello" "hello" $ do
-    it "uses the hello binary" $ do
-      helloPath <- askFile @"hello"
-      readCreateProcess (proc helloPath []) "" >>= (`shouldBe` "Hello, world!\n")
+  introduceNixContext nixpkgsReleaseDefault $
+    introduceBinaryViaNixPackage @"hello" "hello" $ do
+      it "uses the hello binary" $ do
+        useHello
+
+
+useHello :: (MonadIO m, MonadReader context m, HasFile context "hello") => m ()
+useHello = do
+  helloPath <- askFile @"hello"
+  readCreateProcess (proc helloPath []) "" >>= (`shouldBe` "Hello, world!\n")
 
 main :: IO ()
 main = runSandwichWithCommandLineArgs defaultOptions spec
