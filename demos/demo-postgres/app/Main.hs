e7f8f12e9d97f79e133fd6ebda285fa580c3d87d
Tom McLaughlin <tom@codedown.io>
2024-03-10 09:24:31 -0700

    More on postgres context

--- demos/demo-postgres/app/Main.hs
+++ demos/demo-postgres/app/Main.hs
@@ -9,6 +9,8 @@ import Control.Monad.IO.Class
 import Control.Monad.Logger
 import qualified Data.ByteString.Lazy as BL
 import Data.String.Interpolate
+import Database.PostgreSQL.Simple
+import Relude
 import Test.Sandwich
 import Test.Sandwich.Contexts.FakeSmtpServer
 import Test.Sandwich.Contexts.Files
@@ -21,21 +23,36 @@ spec :: TopSpec
 spec = describe "Introducing PostgreSQL" $ do
   describe "Unix socket Via Nix" $
     introduceNixContext nixpkgsReleaseDefault $ introducePostgresUnixSocketViaNix defaultPostgresNixOptions $ do
-      it "prints the MinIO server info" $ do
-        server <- getContext postgres
+      it "prints the server info and does a simple test" $ do
+        server@(PostgresContext {..}) <- getContext postgres
         info [i|Got PostgreSQL server: #{server}|]
+
+        selectTwoPlusTwo (encodeUtf8 postgresConnString) >>= (`shouldBe` 4)
 
   describe "Via Nix" $
     introduceNixContext nixpkgsReleaseDefault $ introducePostgresViaNix defaultPostgresNixOptions $ do
-      it "prints the MinIO server info" $ do
-        server <- getContext postgres
+      it "prints the server info and does a simple test" $ do
+        server@(PostgresContext {..}) <- getContext postgres
         info [i|Got PostgreSQL server: #{server}|]
+
+        selectTwoPlusTwo (encodeUtf8 postgresConnString) >>= (`shouldBe` 4)
 
   describe "Via container" $
     introducePostgresViaContainer defaultPostgresContainerOptions $ do
-      it "prints the MinIO server info" $ do
-        server <- getContext postgres
+      it "prints the server info and does a simple test" $ do
+        server@(PostgresContext {..}) <- getContext postgres
         info [i|Got PostgreSQL server: #{server}|]
+
+        -- TODO: this sometimes fails, is the waitForHealth call not sufficient?
+
+        selectTwoPlusTwo (encodeUtf8 postgresConnString) >>= (`shouldBe` 4)
+
+
+selectTwoPlusTwo :: MonadIO m => ByteString -> m Int
+selectTwoPlusTwo connString = liftIO $ do
+  conn <- connectPostgreSQL connString
+  [Only n] <- query_ conn "select 2 + 2"
+  return n
 
 
 main :: IO ()
