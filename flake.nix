0f04f0b1da45fbb3e81fa3a1bdcae4456da574cc
thomasjm <tom@codedown.io>
2024-08-09 01:52:13 -0700

    Set a working NIX_PATH in .envrc

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     url = "github:hercules-ci/gitignore.nix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
   inputs.nixpkgsMaster.url = "github:NixOS/nixpkgs/master";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
@@ -24,6 +24,8 @@
               export NIX_PATH=nixpkgs=${pkgs.path}
               ${pkgs.stack}/bin/stack test
             '';
+
+            nixpkgsPath = pkgs.writeShellScriptBin "nixpkgsPath.sh" "echo -n ${pkgs.path}";
           };
 
           devShells.default = pkgs.mkShell {
