6f2fbd8ce9393f4f2d0976b6a6f28ad44858ec90
Tom McLaughlin <tom@codedown.io>
2024-10-25 05:26:51 -0700

    Improve openFileExplorerFolderPortable on windows

--- sandwich/src/Test/Sandwich/Formatters/TerminalUI/CrossPlatform.hs
+++ sandwich/src/Test/Sandwich/Formatters/TerminalUI/CrossPlatform.hs
@@ -1,13 +1,23 @@
--- |
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Test.Sandwich.Formatters.TerminalUI.CrossPlatform (
   openFileExplorerFolderPortable
   ) where
 
 import Control.Monad
+import System.Directory
 import System.Process
+
 
 -- | TODO: report exceptions here
 openFileExplorerFolderPortable :: String -> IO ()
+#ifdef mingw32_HOST_OS
 openFileExplorerFolderPortable folder = do
+  findExecutable "explorer.exe" >>= \case
+    Just p -> void $ readCreateProcessWithExitCode (proc p [folder]) ""
+    Nothing -> return ()
+#else
+openFileExplorerFolderPortable folder =
   void $ readCreateProcessWithExitCode (proc "xdg-open" [folder]) ""
+#endif
