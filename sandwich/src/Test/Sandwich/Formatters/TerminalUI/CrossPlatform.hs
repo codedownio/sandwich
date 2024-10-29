436764f7b7efb038095fca1d7531a7ec6d232f49
thomasjm <tom@codedown.io>
2024-10-28 17:54:25 -0700

    CrossPlatform.hs: move Windows-only import under CPP

--- sandwich/src/Test/Sandwich/Formatters/TerminalUI/CrossPlatform.hs
+++ sandwich/src/Test/Sandwich/Formatters/TerminalUI/CrossPlatform.hs
@@ -6,18 +6,21 @@ module Test.Sandwich.Formatters.TerminalUI.CrossPlatform (
   ) where
 
 import Control.Monad
-import System.Directory
 import System.Process
 
 
--- | TODO: report exceptions here
-openFileExplorerFolderPortable :: String -> IO ()
+-- | TODO: report exceptions from this
+
 #ifdef mingw32_HOST_OS
+import System.Directory
+
+openFileExplorerFolderPortable :: String -> IO ()
 openFileExplorerFolderPortable folder = do
   findExecutable "explorer.exe" >>= \case
     Just p -> void $ readCreateProcessWithExitCode (proc p [folder]) ""
     Nothing -> return ()
 #else
+openFileExplorerFolderPortable :: String -> IO ()
 openFileExplorerFolderPortable folder =
   void $ readCreateProcessWithExitCode (proc "xdg-open" [folder]) ""
 #endif
