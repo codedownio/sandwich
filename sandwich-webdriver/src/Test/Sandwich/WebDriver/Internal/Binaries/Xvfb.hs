7852169ead66079f656ae0adce050bc71b6082b0
thomasjm <tom@codedown.io>
2025-01-16 18:39:16 -0800

    sandwich-webdriver: debugging for Binaries/Xvfb.hs

--- sandwich-webdriver/src/Test/Sandwich/WebDriver/Internal/Binaries/Xvfb.hs
+++ sandwich-webdriver/src/Test/Sandwich/WebDriver/Internal/Binaries/Xvfb.hs
@@ -28,10 +28,17 @@ obtainXvfb :: (
   ) => XvfbToUse -> m (Either T.Text FilePath)
 obtainXvfb UseXvfbFromPath = findExecutable "Xvfb" >>= \case
   Nothing -> return $ Left [i|Couldn't find "Xvfb" on the PATH.|]
-  Just p -> return $ Right p
+  Just p -> do
+    debug [i|Found Xvfb at #{p}|]
+    return $ Right p
 obtainXvfb (UseXvfbAt path) = doesFileExist path >>= \case
   False -> return $ Left [i|Path '#{path}' didn't exist|]
-  True -> return $ Right path
-obtainXvfb (UseXvfbFromNixpkgs nixContext) =
+  True -> do
+    debug [i|Found Xvfb at #{path}|]
+    return $ Right path
+obtainXvfb (UseXvfbFromNixpkgs nixContext) = do
   -- Note: on master *after* release-24.05, there seems to be xorg.xvfb
-  Right <$> getBinaryViaNixPackage' @"Xvfb" nixContext "xorg.xorgserver"
+  debug [i|Building Xvfb with Nix|]
+  ret <- getBinaryViaNixPackage' @"Xvfb" nixContext "xorg.xorgserver"
+  debug [i|Built Xvfb: #{ret}|]
+  return $ Right ret
