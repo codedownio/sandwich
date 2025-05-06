2007b638afeac057e056640851a85063d4bd5539
Tom McLaughlin <tom@codedown.io>
2025-05-06 16:39:13 -0700

    ci: add a basic test to sandwich-webdriver

--- sandwich-webdriver/test/Spec.hs
+++ sandwich-webdriver/test/Spec.hs
@@ -6,28 +6,30 @@ import Data.Text as T
 import UnliftIO.Temporary
 import Data.Time.Clock
 import Data.String.Interpolate
+import Test.Sandwich.Contexts.Nix
+import System.FilePath
+import Test.WebDriver.Commands
+import Test.Sandwich.WebDriver
+import Control.Monad.IO.Class
+import qualified Data.ByteString.Lazy as BL
 
 
-spec :: TopSpec
-spec = do
-  it "works" $ do
-    2 `shouldBe` 2
+spec :: TopSpecWithOptions
+spec = introduceNixContext (nixpkgsMaster { nixpkgsDerivationAllowUnfree = True }) $
+  introduceWebDriverViaNix defaultWdOptions $ do
+    it "opens Google and searches" $ withSession1 $ do
+      openPage [i|https://www.google.com|]
+      search <- findElem (ByCSS [i|*[title="Search"]|])
+      click search
+      sendKeys "Haskell Sandwich" search
+      findElem (ByCSS [i|input[type="submit"]|]) >>= click
 
-  -- it "successfully runs obtainChromeDriver" $ do
-  --   withSystemTempDirectory "test-download" $ \dir -> do
-  --     obtainChromeDriver dir (DownloadChromeDriverAutodetect Nothing) >>= \case
-  --       Right x -> info [i|Got chromedriver: #{x}|]
-  --       Left err -> expectationFailure (T.unpack err)
-
-  -- it "successfully runs obtainGeckoDriver" $ do
-  --   withSystemTempDirectory "test-download" $ \dir -> do
-  --     obtainGeckoDriver dir (DownloadGeckoDriverAutodetect Nothing) >>= \case
-  --       Right x -> info [i|Got geckoDriver: #{x}|]
-  --       Left err -> expectationFailure (T.unpack err)
+      Just dir <- getCurrentFolder
+      screenshot >>= liftIO . BL.writeFile (dir </> "screenshot.png")
 
 
 main :: IO ()
-main = runSandwich options spec
+main = runSandwichWithCommandLineArgs options spec
   where
     options = defaultOptions {
       optionsTestArtifactsDirectory = defaultTestArtifactsDirectory
