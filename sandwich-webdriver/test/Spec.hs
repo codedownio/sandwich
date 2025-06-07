ec9b4c27829d74e3c7eed45369ccd418b762cb3d
Tom McLaughlin <tom@codedown.io>
2025-06-07 07:06:55 -0700

    sandwich-webdriver test: try downgrading chrome

--- sandwich-webdriver/test/Spec.hs
+++ sandwich-webdriver/test/Spec.hs
@@ -14,8 +14,13 @@ import Control.Monad.IO.Class
 import qualified Data.ByteString.Lazy as BL
 
 
+-- We use nixpkgsRelease2405 here, because Chrome/chromedriver > 131 seems to have a bug where we get
+-- a message like this in CI:
+--
+-- session not created: probably user data directory is already in use, please specify a unique value for --user-data-dir argument, or don't use --user-data-dir
+
 spec :: TopSpecWithOptions
-spec = introduceNixContext (nixpkgsMaster { nixpkgsDerivationAllowUnfree = True }) $
+spec = introduceNixContext (nixpkgsRelease2405 { nixpkgsDerivationAllowUnfree = True }) $
   introduceWebDriverViaNix defaultWdOptions $ do
     it "opens Google and searches" $ withSession1 $ do
       openPage [i|https://www.google.com|]
