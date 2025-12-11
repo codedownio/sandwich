4e39ba389a6b2dc93b9c925fd3f8d29ba23a8bf5
Tom McLaughlin <tom@codedown.io>
2025-12-11 15:48:10 -0800

    demo-contexts: add a file reading example

--- demos/demo-contexts/app/Main.hs
+++ demos/demo-contexts/app/Main.hs
@@ -6,19 +6,31 @@ module Main where
 
 import Common
 import Control.Monad
+import Control.Monad.IO.Class
+import qualified Data.List as L
 import Data.String.Interpolate
 import Test.Sandwich
 import UnliftIO.Exception
+
+
+-- * Mock database
 
 data DatabaseContext = MySQLDatabaseContext | SqliteDatabaseContext
   deriving Show
 
 database = Label :: Label "database" DatabaseContext
 
-introduceDatabase = introduceWith "Introduce database" database $ \action ->
-  bracket (debug "Spinning up DB..." >> return MySQLDatabaseContext)
-          (\db -> debug "Tearing down DB..." >> return ())
-          (void . action)
+introduceDatabase = introduce "Introduce database" database
+  (debug "Spinning up DB..." >> return MySQLDatabaseContext)
+  (\db -> debug "Tearing down DB..." >> return ())
+
+-- * IO example
+
+fileContents = Label :: Label "fileContents" String
+
+introduceFileContents = introduce "Introduce file contents" fileContents
+  (liftIO $ readFile "demos/demo-contexts/app/Main.hs")
+  (const $ return ())
 
 contextsDemo :: TopSpec
 contextsDemo = describe "Contexts" $ do
@@ -29,6 +41,11 @@ contextsDemo = describe "Contexts" $ do
 
       maybeDb <- getContextMaybe database
       info [i|Got database from getContextMaybe: #{maybeDb}|]
+
+  introduceFileContents $ do
+    it "Uses the file contents" $ do
+      contents <- getContext fileContents
+      info [i|Got fileContents (first 10 lines):\n\n#{L.unlines $ L.take 10 $ L.lines contents}|]
 
   it "Uses a maybe database" $ do
     maybeDb <- getContextMaybe database
