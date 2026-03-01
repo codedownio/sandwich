37442fcf6fe7d662769b81c6429cca289d6cb6fc
Tom McLaughlin <tom@codedown.io>
2026-02-28 17:27:38 -0800

    Just disable logging to debug memory leak

--- sandwich/src/Test/Sandwich/Interpreters/RunTree/Logging.hs
+++ sandwich/src/Test/Sandwich/Interpreters/RunTree/Logging.hs
@@ -19,23 +19,25 @@ import Test.Sandwich.Types.RunTree
 logToMemory :: Maybe LogLevel -> TVar (Seq LogEntry) -> Loc -> LogSource -> LogLevel -> LogStr -> IO ()
 logToMemory Nothing _ _ _ _ _ = return ()
 logToMemory (Just minLevel) logs loc logSrc logLevel logStr =
-  when (logLevel >= minLevel) $ do
-    ts <- getCurrentTime
-    let !bs = fromLogStr logStr
-    atomically $ modifyTVar' logs (|> LogEntry ts loc logSrc logLevel bs)
+  return ()
+  -- when (logLevel >= minLevel) $ do
+  --   ts <- getCurrentTime
+  --   let !bs = fromLogStr logStr
+  --   atomically $ modifyTVar' logs (|> LogEntry ts loc logSrc logLevel bs)
 
 logToMemoryAndFile :: Maybe LogLevel -> Maybe LogLevel -> LogEntryFormatter -> TVar (Seq LogEntry) -> Handle -> Loc -> LogSource -> LogLevel -> LogStr -> IO ()
 logToMemoryAndFile maybeMemLogLevel maybeSavedLogLevel formatter logs h loc logSrc logLevel logStr = do
-  let !bs = fromLogStr logStr
-  maybeTs <- case maybeMemLogLevel of
-    Just x | x <= logLevel -> do
-      ts <- getCurrentTime
-      atomically $ modifyTVar' logs (|> LogEntry ts loc logSrc logLevel bs)
-      return $ Just ts
-    _ -> return Nothing
+  return ()
+  -- let !bs = fromLogStr logStr
+  -- maybeTs <- case maybeMemLogLevel of
+  --   Just x | x <= logLevel -> do
+  --     ts <- getCurrentTime
+  --     atomically $ modifyTVar' logs (|> LogEntry ts loc logSrc logLevel bs)
+  --     return $ Just ts
+  --   _ -> return Nothing
 
-  case maybeSavedLogLevel of
-    Just x | x <= logLevel -> do
-      ts <- maybe getCurrentTime return maybeTs
-      BS8.hPutStr h $ formatter ts loc logSrc logLevel bs
-    _ -> return ()
+  -- case maybeSavedLogLevel of
+  --   Just x | x <= logLevel -> do
+  --     ts <- maybe getCurrentTime return maybeTs
+  --     BS8.hPutStr h $ formatter ts loc logSrc logLevel bs
+  --   _ -> return ()
