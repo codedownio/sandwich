e0b8003548938b29f2f739fe93989730a7425e98
Tom McLaughlin <tom@codedown.io>
2026-02-28 18:00:25 -0800

    Try forcing seqs

--- sandwich/src/Test/Sandwich/Interpreters/RunTree/Logging.hs
+++ sandwich/src/Test/Sandwich/Interpreters/RunTree/Logging.hs
@@ -16,28 +16,31 @@ import Data.Time.Clock
 import System.IO
 import Test.Sandwich.Types.RunTree
 
+forceSeq :: Seq LogEntry -> Seq LogEntry
+forceSeq s = foldl' (\_ e -> e `seq` ()) () s `seq` s
+
 logToMemory :: Maybe LogLevel -> TVar (Seq LogEntry) -> Loc -> LogSource -> LogLevel -> LogStr -> IO ()
 logToMemory Nothing _ _ _ _ _ = return ()
 logToMemory (Just minLevel) logs loc logSrc logLevel logStr =
-  return ()
-  -- when (logLevel >= minLevel) $ do
-  --   ts <- getCurrentTime
-  --   let !bs = fromLogStr logStr
-  --   atomically $ modifyTVar' logs (|> LogEntry ts loc logSrc logLevel bs)
+  when (logLevel >= minLevel) $ do
+    ts <- getCurrentTime
+    let !bs = fromLogStr logStr
+    let !entry = LogEntry ts loc logSrc logLevel bs
+    atomically $ modifyTVar' logs (\s -> let s' = s |> entry in forceSeq s' `seq` s')
 
 logToMemoryAndFile :: Maybe LogLevel -> Maybe LogLevel -> LogEntryFormatter -> TVar (Seq LogEntry) -> Handle -> Loc -> LogSource -> LogLevel -> LogStr -> IO ()
 logToMemoryAndFile maybeMemLogLevel maybeSavedLogLevel formatter logs h loc logSrc logLevel logStr = do
-  return ()
-  -- let !bs = fromLogStr logStr
-  -- maybeTs <- case maybeMemLogLevel of
-  --   Just x | x <= logLevel -> do
-  --     ts <- getCurrentTime
-  --     atomically $ modifyTVar' logs (|> LogEntry ts loc logSrc logLevel bs)
-  --     return $ Just ts
-  --   _ -> return Nothing
+  let !bs = fromLogStr logStr
+  maybeTs <- case maybeMemLogLevel of
+    Just x | x <= logLevel -> do
+      ts <- getCurrentTime
+      let !entry = LogEntry ts loc logSrc logLevel bs
+      atomically $ modifyTVar' logs (\s -> let s' = s |> entry in forceSeq s' `seq` s')
+      return $ Just ts
+    _ -> return Nothing
 
-  -- case maybeSavedLogLevel of
-  --   Just x | x <= logLevel -> do
-  --     ts <- maybe getCurrentTime return maybeTs
-  --     BS8.hPutStr h $ formatter ts loc logSrc logLevel bs
-  --   _ -> return ()
+  case maybeSavedLogLevel of
+    Just x | x <= logLevel -> do
+      ts <- maybe getCurrentTime return maybeTs
+      BS8.hPutStr h $ formatter ts loc logSrc logLevel bs
+    _ -> return ()
